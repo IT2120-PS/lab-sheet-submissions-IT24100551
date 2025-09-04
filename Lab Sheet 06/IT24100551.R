e84ec4df4a314f175bbb2e4e1be0efa86733e336
IT24100551 <it24100551@my.sliit.lk>
2025-09-04 22:41:50 +0530

    IT24100551_Lab06

---
+++ Lab Sheet 06/IT24100551.R
@@ -0,0 +1,22 @@
+
+setwd("C:\\Users\\it24100551\\Desktop")
+
+#01(1)
+n <- 50
+p <- 0.85
+cat("X ~ Binomial(",n,",",p,")")
+
+#01(02)
+p1 <- pbinom(46,n,p)
+p1
+
+#02(01)
+# X = Number of calls received in one hour
+
+#02(02)
+lamda <- 12
+cat("X ~ Poisson (", lamda,")")
+
+#02(03)
+dpois(15,lambda = lamda)
+
